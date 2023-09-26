e1891acd7dad38279c55e44f4562b8907aa98723
HaabuK <karl.h.haabu@gmail.com>
2023-09-26 13:17:20 +0300

    Update Startup.cs

--- ContosoUniversity/ContosoUniversity/Startup.cs
+++ ContosoUniversity/ContosoUniversity/Startup.cs
@@ -30,6 +30,8 @@ namespace ContosoUniversity
             services.AddDbContext<SchoolContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
             services.AddControllersWithViews();
         }
 
