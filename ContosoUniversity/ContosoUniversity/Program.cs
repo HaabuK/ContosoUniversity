234bd12c41eb2bfbe0c286aedc405084d6149da6
HaabuK <karl.h.haabu@gmail.com>
2023-09-26 13:20:31 +0300

    Update Program.cs

--- ContosoUniversity/ContosoUniversity/Program.cs
+++ ContosoUniversity/ContosoUniversity/Program.cs
@@ -13,9 +13,20 @@ namespace ContosoUniversity
         {
             var host = CreateHostBuilder(args).Build();
 
-            CreateDbIfNotExists(host);
-
-            host.Run();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
         }
 
         private static void CreateDbIfNotExists(IHost host)
